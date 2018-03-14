3b6a464db3d08801a423407e6afb611489646754
briankeane <lonesomewhistle@gmail.com>
2018-03-14 11:19:12 -0500

    debug

--- routes/v1/subroutes/songRequests.js
+++ routes/v1/subroutes/songRequests.js
@@ -63,6 +63,8 @@ function initiateAcquisition(req, res, next) {
 }
 
 function checkBucket(req, res, next) {
+  console.log('req.body.bucketName', req.body.bucketName)
+  console.log('process.en.SONGS_BUCKET_NAME', process.env.SONGS_BUCKET_NAME)
   if (req.body.bucketName !== process.env.SONGS_BUCKET_NAME) {
     return res.status(400).json({ message: errors.INVALID_BUCKET_NAME })
   }
