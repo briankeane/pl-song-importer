8711c8fbca026a9838f001aaf9dfdf08df7471f9
briankeane <lonesomewhistle@gmail.com>
2018-03-17 12:33:38 -0500

    migrate db, adjust db interface for songRequests

--- migrations/1521303625239_migration.js
+++ migrations/1521303625239_migration.js
@@ -1,24 +1,11 @@
 exports.up = function(pgm) {
   pgm.addColumns('song_requests', 
     {
-      'spotify_info_error' : 
-      {type: 'jsonb', default: false}
-    },
-    {
-      'youtube_matches_error' : 
-      {type: 'jsonb', default: false}
-    },
-    {
-      'youtube_matches' :
-      {type: 'jsonb[]', default: false}
-    },
-    {
-      'key' :
-      {type: 'string', default: false}
-    },
-    {
-      'download_error' :
-      {type: 'jsonb', default: false}
+      'spotify_info_error' : {type: 'jsonb'},
+      'youtube_matches_error' : {type: 'jsonb'},
+      'youtube_matches' : {type: 'jsonb[]'},
+      'key' : {type: 'string'},
+      'download_error' : {type: 'jsonb'}
     }
   )
 };
